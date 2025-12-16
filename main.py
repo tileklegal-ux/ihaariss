16ff04aa842883bbbe3a043fb40608a036dbe252
tileklegal-ux <tileklegal@gmail.com>
2025-12-17 05:55:32 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,51 @@
 # main.py
 import logging
-from telegram.ext import Application
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+
+from config import TELEGRAM_TOKEN
+from database.db import get_user_role
+
+from handlers.user import cmd_start_user
+from handlers.owner import owner_start
+from handlers.manager import manager_start
 
 from handlers.user import register_handlers_user
 from handlers.owner import register_handlers_owner
 from handlers.manager import register_handlers_manager
-
-from config import TELEGRAM_TOKEN
 
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
 )
 
+async def start_router(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    role = get_user_role(update.effective_user.id)
+
+    if role == "owner":
+        await owner_start(update, context)
+        return
+
+    if role == "manager":
+        await manager_start(update, context)
+        return
+
+    await cmd_start_user(update, context)
+
+
 def main():
     app = Application.builder().token(TELEGRAM_TOKEN).build()
 
-    # Порядок ВАЖЕН
-    # owner / manager идут раньше user
+    # ЕДИНСТВЕННЫЙ /start
+    app.add_handler(CommandHandler("start", start_router), group=0)
+
+    # остальные хендлеры
     register_handlers_owner(app)    # group 1
     register_handlers_manager(app)  # group 2
     register_handlers_user(app)     # group 4
 
     app.run_polling()
+
 
 if __name__ == "__main__":
     main()
