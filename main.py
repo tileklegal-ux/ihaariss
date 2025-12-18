0cfeb87583c76120372b75b4b9fad69fc01476a2
tileklegal-ux <tileklegal@gmail.com>
2025-12-18 22:04:58 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 from telegram.ext import Application
 
-from handlers.owner import register_handlers_owner
+from config import BOT_TOKEN
 from database.db import init_db
-from user import register_user_handlers  # твой user.py
+
+from handlers.start import register_start_handlers
+from handlers.user import register_user_handlers
+from handlers.owner import register_handlers_owner
 
 
 def main():
     init_db()
 
-    app = Application.builder().token("BOT_TOKEN").build()
+    app = Application.builder().token(BOT_TOKEN).build()
 
+    # /start + init user
+    register_start_handlers(app)
+
+    # user FSM / premium / ai
     register_user_handlers(app)
+
+    # owner / manager
     register_handlers_owner(app)
 
     app.run_polling()
