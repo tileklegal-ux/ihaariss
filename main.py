121765e5bc36fab70dba6adea0414ce65d002690
tileklegal-ux <tileklegal@gmail.com>
2025-12-18 23:05:20 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,12 @@
-from telegram.ext import Application
+from telegram.ext import Application, CommandHandler
 
 from config import BOT_TOKEN
 from handlers.start import register_start_handlers
 
 def main():
-    application = Application.builder().token(BOT_TOKEN).build()
-
-    # /start (роутер по ролям)
-    register_start_handlers(application)
-
-    # ❗ ВАЖНО:
-    # user.py НЕ регистрируется тут как отдельный модуль
-    # Все text handlers живут внутри user.text_router()
-
-    application.run_polling()
-
+    app = Application.builder().token(BOT_TOKEN).build()
+    register_start_handlers(app)
+    app.run_polling()
 
 if __name__ == "__main__":
     main()
